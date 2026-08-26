76063360cebcaacd3bd7cb34cac55ff4f216b5a9
Rafael M. <58067217+rafleo2008@users.noreply.github.com>
2026-08-26 14:40:42 -0500

    Extender Auxiliar

--- Auxiliar_RDS.R
+++ Auxiliar_RDS.R
@@ -7,7 +7,21 @@ rutas <- st_read("Assets/GeoJSON/09_Ruta_sabanas_id_hexagonoV2.geojson") %>%
 poligonosV2 <- st_read("Assets/GeoJSON/10_Hexagonos_clusteres_filtrados.geojson") %>% 
   st_transform(4326)
 
+patios_ele_buff <- st_read("Assets/GeoJSON/11_Buffer_3km_Patios_electricos.geojson")%>%
+  st_transform(4326)
+patios_ele_punt <-st_read("Assets/GeoJSON/12_Patios_electricos_TM.geojson")%>%
+  st_transform(4326)
+punt_ad_buff <-st_read("Assets/GeoJSON/13_Buffer_3km_Estaciones_adicionales.geojson")%>%
+  st_transform(4326) 
+punt_ad_punt <-st_read("Assets/GeoJSON/14_Cargadores_adicionales.geojson")%>%
+  st_transform(4326)
+
 # 2. Guardar versión RDS en una carpeta Assets/RDS
 dir.create("Assets/RDS", showWarnings = FALSE)
 saveRDS(rutas, "Assets/RDS/rutas.rds")
 saveRDS(poligonosV2, "Assets/RDS/poligonosV2.rds")
+
+saveRDS(patios_ele_buff, "Assets/RDS/P_Elec_buff.rds")
+saveRDS(patios_ele_punt, "Assets/RDS/P_Elec_punt.rds")
+saveRDS(punt_ad_buff, "Assets/RDS/P_Ad_buff.rds")
+saveRDS(punt_ad_punt, "Assets/RDS/P_Ad_punt.rds")
